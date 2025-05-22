308323bd01915fdbd8653e3567c112c2c8548b46
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2025-05-22 09:26:19 +0400

    commit

---
+++ Level 29/homework/script3.py
@@ -0,0 +1,30 @@
+"""შეისწავლეთ chr და ord Python-ში, რესურსები:"""
+"""
+x = chr(97)
+
+print(x)
+
+y = chr(107)
+
+print(y)
+
+print(chr(97))
+
+print(chr(107))
+
+print(chr(105))
+"""
+
+"""
+x = ord("h")
+
+print(x)
+
+y = ord("g")
+
+print(y)
+
+print(ord("a"))
+
+print(ord("g"))
+"""
