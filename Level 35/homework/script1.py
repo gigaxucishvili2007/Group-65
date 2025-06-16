b3ee4b3df9bbcce00dd39032a3d72f1c01851d65
gigaxucishvili2007 <akaki.xuca12345@gmail.com>
2025-06-16 10:59:31 +0400

    commit

---
+++ Level 35/homework/script1.py
@@ -0,0 +1,14 @@
+"""1. "გადათარგმნეთ" ფსევდო კოდი Python-ზე."""
+
+"""start
+repeat until authorised = true
+INPUT log_in
+access_granted = true"""
+
+name = input("Enter ur name :")
+lastname = input("Enter a lastname")
+age = int(input("Enter your age"))
+print(name)
+print(lastname)
+print(age)
+print("Registration is succesfull 🙌")
---
+++ Level 35/homework/script2.py
@@ -0,0 +1,7 @@
+"""3. დაწერეთ ამ ლოგიკით კოდი Python-ში:"""
+
+age = int(input("Enter your age"))
+if age > 18:
+    print("You are adult 👨‍🦰")
+else:
+    print("You are not adult 👶")
